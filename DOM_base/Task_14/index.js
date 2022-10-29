1eb3bb0be6252b7f7174530f62821bdea1fc0d14
Andrei Lopatko <andrei.lopatko@together.com>
2022-10-29 16:01:24 +0300

    Add types for vars

--- DOM_base/Task_14/index.js
+++ DOM_base/Task_14/index.js
@@ -1,19 +1,57 @@
 // Task_14
-import putElement from "../../helpers/putElement.js";
+import { putElement } from "../../helpers/putElement.js";
 
 const innerElement = `
   <div class="inner">Inner element</div>
 `;
 
 putElement(document.body, {
-  child: innerElement,
-  class: "outer",
-  tag: "img",
+  tag: "input",
   attributes: {
-    src: "https://i.picsum.photos/id/1018/200/300.jpg?hmac=IrYgAIczHOxOgmWliW3MlASD3LdAJ_aHAdh5f2aY9Sw"
-  }
+    src: "https://i.picsum.photos/id/1018/200/300.jpg?hmac=IrYgAIczHOxOgmWliW3MlASD3LdAJ_aHAdh5f2aY9Sw",
+  },
 });
 
+// number;
+// string;
+// boolean;
+// undefined;
+// null;
+// object;
+// Symbol;
+// BigInt()
+
+const a = { b: 15 };
+
+// falsey = 0, "", null, undefined, false, NaN
 
 
+const result = lol() || kek() || kek() || lol();
 
+console.log("result: ", result);
+
+if(result)  {
+  console.log("hey");
+} else {
+  console.log("hoy");
+}
+
+function lol() {
+  console.log("lol");
+  return 4;
+}
+
+function kek() {
+  console.log("kek");
+  return 6;
+}
+
+
+// const tag_1 = options.tag || "div";
+//
+// let tag;
+// if(options.tag === undefined) {
+//   tag =  "div";
+// } else {
+//   tag = options.tag;
+// }
--- helpers/putElement.js
+++ helpers/putElement.js
@@ -1,17 +1,17 @@
-export default function putElement(parent, options) {
-  const singleTags = ["input", "img"];
-  const tag = options.tag ?? "div"; // Ленивое применение || &&
+export function putElement(parent, options) {
+  const singleTags = ["input", "img", "video", "audio"];
+  const tag = options.tag ?? "div";
   const child = options.child ?? "";
-  const className = options.class ? `class=${options.class}` : "";  // Тернарный оператор
-  const attributes = options.attributes ? mapAttributes(options.attributes) : "";
   const insertPlace = options.place ?? "beforeend";
+  const className = options.class ? `class="${options.class}"` : "";
+  const attributes = options.attributes ? mapAttributes(options.attributes) : "";
   let element = `
     <${tag} ${className} ${attributes}>
       ${child}
     </${tag}>
   `;
-  if (singleTags.includes(tag)) {  // Проверка, входит ли tag в массив
-    element = `<${tag} ${className} ${attributes} />`;
+  if (singleTags.includes(tag)) {
+    element = `<${tag} ${className} ${attributes}>`;
   }
   parent.insertAdjacentHTML(insertPlace, element);
 }
