23a8ecce4bd6ef2d4b48902e39f261c78499a2d6
Andrei Lopatko <andrei.lopatko@together.com>
2022-11-01 23:05:30 +0200

    Add map and filter difficulty cases

--- DOM_base/Task_14/arr_map.js
+++ DOM_base/Task_14/arr_map.js
@@ -24,6 +24,37 @@ function filter (arr, cb) {
 }
 
 const filterArr = filter(source, (item, index, arr) => {
-    return item>100
+    return item > 100;
 })
-console.log(filterArr)
+
+const users = [
+    { name: "lol", age: 5, height: 158, gender: "male" },
+    { name: "vas", age: 15, height: 178, gender: "male" },
+    { name: "top", age: 18, height: 128, gender: "female" },
+    { name: "frt", age: 27, height: 108, gender: "male" },
+    { name: "ewq", age: 14, height: 198, gender: "female" },
+    { name: "vvbgh", age: 10, height: 148, gender: "male" },
+    { name: "sdfs", age: 12, height: 138, gender: "male" },
+    { name: "sdfsf", age: 8, height: 188, gender: "female" },
+];
+
+const heightStudy = users.filter((user, index, arr) => {
+    return user.height > 140;
+});
+
+const htmlUsers = users.map((user) => {
+    return `
+        <div class="card ${user.gender}">
+          <h2>${user.name}</h2>
+          <p>age: ${user.age}</p>
+          <p>height: ${user.height}</p>
+        </div>
+    `;
+});
+
+document.body.insertAdjacentHTML("beforeend", htmlUsers.join(""));
+
+console.log(users.map((user) => user.name));
+
+
+
