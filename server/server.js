236ac2baaee5d9d35c68281bf2ab81f49dc45886
Andrei Lopatko <andrei.lopatko@together.com>
2022-12-28 00:41:55 +0200

    Add function tasks

--- server/server.js
+++ server/server.js
@@ -2,20 +2,15 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  if (req.headers.accept.includes("json")) {
-    fs.readFile('./index.html', (err, html) => {
-      if (err) {
-        res.statusCode = 505;
-        res.end();
-        throw err;
-      }
-      res.write(html);
+  fs.readFile('./index.html', (err, html) => {
+    if (err) {
+      res.statusCode = 505;
       res.end();
-    });
-  } else {
-    res.statusCode = 201;
+      throw err;
+    }
+    res.write(html);
     res.end();
-  }
+  });
 });
 
 server.listen(8080, () => {
